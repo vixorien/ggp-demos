1c3e1f3d136959c44886f1e381f2815d4feda948
Chris Cascioli <chriscascioli@gmail.com>
2023-03-27 16:43:00 -0400

    Added UI, including light-specific UI, to basic lighting demo

--- 08 - Basic Lighting/Game.h
+++ 08 - Basic Lighting/Game.h
@@ -32,10 +32,24 @@ private:
 	// Initialization helper methods - feel free to customize, combine, etc.
 	void LoadAssetsAndCreateEntities();
 
+	// UI functions
+	void UINewFrame(float deltaTime);
+	void BuildUI();
+	void CameraUI(std::shared_ptr<Camera> cam);
+	void EntityUI(std::shared_ptr<GameEntity> entity);
+	void MaterialUI(std::shared_ptr<Material> material);
+	void LightUI(Light& light);
+
+	// Should the ImGui demo window be shown?
+	bool showUIDemoWindow;
+
 	// Camera for the 3D scene
 	std::shared_ptr<Camera> camera;
 
-	// Storage for scene data
+
+	// Vectors for scene data
+	std::vector<std::shared_ptr<Mesh>> meshes;
+	std::vector<std::shared_ptr<Material>> materials;
 	std::vector<std::shared_ptr<GameEntity>> entities;
 	std::vector<Light> lights;
 	DirectX::XMFLOAT3 ambientColor;
