f596b6b676b40ce09de6698212955f08a4b9a1e8
Chris Cascioli <chriscascioli@gmail.com>
2025-01-13 14:31:46 -0500

    Moved all GGP projects into GGP subfolder

--- GGP/Common/AssetPath.h
+++ GGP/Common/AssetPath.h
@@ -3,4 +3,4 @@
 // The relative path to the assets folder for all demos.
 // This allows for easy updating should the overall
 // folder structure change later.
-std::wstring AssetPath = L"../../../Assets/";
+std::wstring AssetPath = L"../../../../Assets/";
