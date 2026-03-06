c74769a97c69315044eff9f415850a61dbd91238
Chris Cascioli <chriscascioli@gmail.com>
2026-03-06 11:43:34 -0500

    Cleaned up second DXR demo using changes from first

--- GGP2/DXR/01 - Multiple Objects/Raytracing.hlsl
+++ GGP2/DXR/01 - Multiple Objects/Raytracing.hlsl
@@ -11,9 +11,6 @@ struct Vertex
     float3 tangent;
 };
 
-// 11 floats total per vertex * 4 bytes each
-static const uint VertexSizeInBytes = 11 * 4; 
-
 struct SceneData
 {
 	matrix InverseViewProjection;
@@ -51,30 +48,6 @@ cbuffer DrawData : register(b0)
 	uint OutputUAVDescriptorIndex;
 };
 
-//cbuffer SceneDataCB : register(b0)
-//{
-//	matrix inverseViewProjection;
-//	float3 cameraPosition;
-//};
-
-//cbuffer ObjectData : register(b1)
-//{
-//	float4 entityColor;	
-//}
-
-
-// === Resources ===
-
-//// Output UAV 
-//RWTexture2D<float4> OutputColor				: register(u0);
-
-//// The actual scene we want to trace through (a TLAS)
-//RaytracingAccelerationStructure SceneTLAS	: register(t0);
-
-//// Geometry buffers
-//ByteAddressBuffer IndexBuffer        		: register(t1);
-//ByteAddressBuffer VertexBuffer				: register(t2);
-
 
 // === Helpers ===
 
@@ -94,65 +67,61 @@ uint3 LoadIndices(uint triangleIndex)
 // Barycentric interpolation of data from the triangle's vertices
 Vertex InterpolateVertices(uint triangleIndex, float2 barycentrics)
 {
+	// Get the data for this entity
+	StructuredBuffer<EntityData> ed = ResourceDescriptorHeap[EntityDataDescriptorIndex];
+	EntityData thisEntity = ed[InstanceIndex()];
+	
+	// Get the geometry buffers
+	StructuredBuffer<uint> IndexBuffer = ResourceDescriptorHeap[thisEntity.IndexBufferDescriptorIndex];
+	StructuredBuffer<Vertex> VertexBuffer = ResourceDescriptorHeap[thisEntity.VertexBufferDescriptorIndex];
+
+	// Grab the 3 indices for this triangle
+	uint firstIndex = triangleIndex * 3;
+	uint indices[3];
+	indices[0] = IndexBuffer[firstIndex + 0];
+	indices[1] = IndexBuffer[firstIndex + 1];
+	indices[2] = IndexBuffer[firstIndex + 2];
+
+	// Grab the 3 corresponding vertices
+	Vertex verts[3];
+	verts[0] = VertexBuffer[indices[0]];
+	verts[1] = VertexBuffer[indices[1]];
+	verts[2] = VertexBuffer[indices[2]];
+	
 	// Calculate the barycentric data for vertex interpolation
 	float3 barycentricData = float3(
 		1.0f - barycentrics.x - barycentrics.y,
 		barycentrics.x,
 		barycentrics.y);
-
-	// Grab the indices
-	uint3 indices = LoadIndices(triangleIndex);
-
-	// Set up the final vertex
-    Vertex vert = (Vertex)0;
 	
-	StructuredBuffer<EntityData> dataBuffer = ResourceDescriptorHeap[EntityDataDescriptorIndex];
-	ByteAddressBuffer VertexBuffer = ResourceDescriptorHeap[dataBuffer[InstanceIndex()].VertexBufferDescriptorIndex];
-
 	// Loop through the barycentric data and interpolate
+    Vertex finalVert = (Vertex)0;
 	for (uint i = 0; i < 3; i++)
 	{
-		// Get the index of the first piece of data for this vertex
-		uint dataIndex = indices[i] * VertexSizeInBytes;
-
-		// Grab the position and offset
-		vert.localPosition += asfloat(VertexBuffer.Load3(dataIndex)) * barycentricData[i];
-		dataIndex += 3 * 4; // 3 floats * 4 bytes per float
-
-		// UV
-		vert.uv += asfloat(VertexBuffer.Load2(dataIndex)) * barycentricData[i];
-		dataIndex += 2 * 4; // 2 floats * 4 bytes per float
-
-		// Normal
-		vert.normal += asfloat(VertexBuffer.Load3(dataIndex)) * barycentricData[i];
-		dataIndex += 3 * 4; // 3 floats * 4 bytes per float
-
-		// Tangent (no offset at the end, since we start over after looping)
-		vert.tangent += asfloat(VertexBuffer.Load3(dataIndex)) * barycentricData[i];
+		finalVert.localPosition += verts[i].localPosition * barycentricData[i];
+		finalVert.uv += verts[i].uv * barycentricData[i];
+		finalVert.normal += verts[i].normal * barycentricData[i];
+		finalVert.tangent += verts[i].tangent * barycentricData[i];
 	}
-
-	// Final interpolated vertex data is ready
-	return vert;
+	return finalVert;
 }
 
 
 // Calculates an origin and direction from the camera for specific pixel indices
-RayDesc CalcRayFromCamera(float2 rayIndices)
+RayDesc CalcRayFromCamera(float2 rayIndices, float3 camPos, float4x4 invVP)
 {
-	ConstantBuffer<SceneData> cb = ResourceDescriptorHeap[SceneDataCBIndex];
-	
 	// Offset to the middle of the pixel
 	float2 pixel = rayIndices + 0.5f;
 	float2 screenPos = pixel / DispatchRaysDimensions().xy * 2.0f - 1.0f;
 	screenPos.y = -screenPos.y;
 
 	// Unproject the coords
-	float4 worldPos = mul(cb.InverseViewProjection, float4(screenPos, 0, 1));
+	float4 worldPos = mul(invVP, float4(screenPos, 0, 1));
 	worldPos.xyz /= worldPos.w;
 
 	// Set up the ray
 	RayDesc ray;
-	ray.Origin = cb.CameraPosition.xyz;
+	ray.Origin = camPos.xyz;
 	ray.Direction = normalize(worldPos.xyz - ray.Origin);
 	ray.TMin = 0.01f;
 	ray.TMax = 1000.0f;
@@ -167,12 +136,18 @@ RayDesc CalcRayFromCamera(float2 rayIndices)
 [shader("raygeneration")]
 void RayGen()
 {
+	// Grab the constant buffer
+	ConstantBuffer<SceneData> cb = ResourceDescriptorHeap[SceneDataCBIndex];
+	
 	// Get the ray indices
 	uint2 rayIndices = DispatchRaysIndex().xy;
 
 	// Calculate the ray from the camera through a particular
 	// pixel of the output buffer using this shader's indices
-	RayDesc ray = CalcRayFromCamera(rayIndices);
+	RayDesc ray = CalcRayFromCamera(
+		rayIndices,
+		cb.CameraPosition,
+		cb.InverseViewProjection);
 
 	// Set up the payload for the ray
 	// This initializes the struct to all zeros
@@ -215,11 +190,12 @@ void Miss(inout RayPayload payload)
 void ClosestHit(inout RayPayload payload, BuiltInTriangleIntersectionAttributes hitAttributes)
 {
 	// Not specifically using any triangle data here, but for reference:
-	//uint triangleIndex = PrimitiveIndex();
-	//Vertex interpolatedVert = InterpolateVertices(triangleIndex, hitAttributes.barycentrics);
-	//payload.color = interpolatedVert.normal;
+	//Vertex interpolatedVert = InterpolateVertices(
+	//	PrimitiveIndex(), 
+	//	hitAttributes.barycentrics);
 	
 	// Get the data for this entity
-	StructuredBuffer<EntityData> dataBuffer = ResourceDescriptorHeap[EntityDataDescriptorIndex];
-	payload.color = dataBuffer[InstanceIndex()].Color.rgb;
+	StructuredBuffer<EntityData> entityDataBuffer = ResourceDescriptorHeap[EntityDataDescriptorIndex];
+	EntityData thisEntity = entityDataBuffer[InstanceIndex()];
+	payload.color = thisEntity.Color.rgb;
 }
