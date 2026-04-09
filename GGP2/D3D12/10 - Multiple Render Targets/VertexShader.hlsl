b2a9444b0c3790e5aa337e1a9a58c26c20280f5d
Chris Cascioli <chriscascioli@gmail.com>
2026-04-08 22:39:17 -0400

    Initial MRT demo commit (no actual MRTs yet)

---
+++ GGP2/D3D12/10 - Multiple Render Targets/VertexShader.hlsl
@@ -0,0 +1,70 @@
+
+cbuffer BindlessData : register(b0)
+{
+	uint vsVertexBufferIndex;
+	uint vsPerFrameCBIndex;
+	uint vsPerObjectCBIndex;
+	uint psPerFrameCBIndex;
+	uint psPerObjectCBIndex;
+}
+
+struct VSPerFrameData
+{
+	matrix view;
+	matrix projection;
+};
+
+struct VSPerObjectData
+{
+	matrix world;
+	matrix worldInverseTranspose;
+};
+
+// Struct representing a single vertex worth of data
+struct Vertex
+{
+	float3 localPosition	: POSITION;
+	float2 uv				: TEXCOORD;
+	float3 normal			: NORMAL;
+	float3 tangent			: TANGENT;
+};
+
+// Struct representing the data we're sending down the pipeline
+struct VertexToPixel
+{
+	float4 screenPosition	: SV_POSITION;
+	float2 uv				: TEXCOORD;
+	float3 normal			: NORMAL;
+	float3 tangent			: TANGENT;
+	float3 worldPos			: POSITION;
+};
+
+// --------------------------------------------------------
+// The entry point (main method) for our vertex shader
+// --------------------------------------------------------
+VertexToPixel main(uint vertexID : SV_VertexID)
+{
+	ConstantBuffer<VSPerFrameData> cbFrame = ResourceDescriptorHeap[vsPerFrameCBIndex];
+	ConstantBuffer<VSPerObjectData> cbObject = ResourceDescriptorHeap[vsPerObjectCBIndex];
+	StructuredBuffer<Vertex> vb = ResourceDescriptorHeap[vsVertexBufferIndex];
+	Vertex vert = vb[vertexID];
+	
+	// Set up output struct
+	VertexToPixel output;
+
+	// Calc screen position
+	matrix wvp = mul(cbFrame.projection, mul(cbFrame.view, cbObject.world));
+	output.screenPosition = mul(wvp, float4(vert.localPosition, 1.0f));
+
+	// Make sure the lighting vectors are in world space
+	output.normal = normalize(mul((float3x3)cbObject.worldInverseTranspose, vert.normal));
+	output.tangent = normalize(mul((float3x3)cbObject.world, vert.tangent));
+
+	// Calc vertex world pos
+	output.worldPos = mul(cbObject.world, float4(vert.localPosition, 1.0f)).xyz;
+
+	// Pass through the uv
+	output.uv = vert.uv;
+
+	return output;
+}
---
+++ GGP2/D3D12/10 - Multiple Render Targets/Window.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <Windows.h>
+#include <string>
+
+namespace Window
+{
+	// Getters
+	unsigned int Width();
+	unsigned int Height();
+	float AspectRatio();
+	HWND Handle();
+	bool HasFocus();
+	bool IsMinimized();
+
+	// Window-related functions
+	HRESULT Create(
+		HINSTANCE appInstance,
+		unsigned int width,
+		unsigned int height,
+		std::wstring titleBarText,
+		bool statsInTitleBar,
+		void (*resizeCallback)());
+	void UpdateStats(float totalTime);
+	void Quit();
+
+	// Helper function for allocating a console window
+	void CreateConsoleWindow(int bufferLines, int bufferColumns, int windowLines, int windowColumns);
+
+	// OS-level message handling
+	LRESULT ProcessMessage(
+		HWND hWnd,
+		UINT uMsg,
+		WPARAM wParam,
+		LPARAM lParam);
+}
